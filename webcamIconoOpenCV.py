c20c1d99d60a84b7267c2148073fae2f635317e6
Gonzalo López Castilla <gonzalo@coderei.dev>
2023-05-29 15:30:26 +0200

    fix: detection added

--- webcamIconoOpenCV.py
+++ webcamIconoOpenCV.py
@@ -2,19 +2,43 @@ import cv2
 
 video_capture = cv2.VideoCapture(0)
 
+logo = cv2.imread('opencv.png', cv2.IMREAD_UNCHANGED)
+
 while True:
     ret, background = video_capture.read()
-
-    primer_plano = background[:, :, 0:3]
-    altura_primer_plano, anchura_primer_plano, _ = primer_plano.shape
-
-    cv2.imshow('MEZCLA', background)
 
     (corners, ids, rejected) = cv2.aruco.detectMarkers(background,
                                                        cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_100),
                                                        parameters=cv2.aruco.DetectorParameters())
-    print(corners)
-    print(ids)
+
+    marca_de_agua = cv2.resize(logo, None, fx=0.34, fy=0.3)
+
+    primer_plano = background[:, :, 0:3]
+    altura_primer_plano, anchura_primer_plano, _ = primer_plano.shape
+
+    minilog = marca_de_agua[:, :, 0:3]
+    altura_logo, anchura_logo, _ = minilog.shape
+    alfa = marca_de_agua[:, :, 3]
+    inverso_alfa = 255 - alfa  # inverso de alfa
+    alfa = cv2.cvtColor(alfa, cv2.COLOR_GRAY2BGR) / 255
+    inverso_alfa = cv2.cvtColor(inverso_alfa, cv2.COLOR_GRAY2BGR) / 255
+
+    if len(corners) > 0:
+        primer_parametro = int(corners[0][0][2][1]) - altura_logo
+        segundo_parametro = int(corners[0][0][2][1])
+        tercer_parametro = int(corners[0][0][1][0]) - anchura_logo
+        cuarto_parametro = int(corners[0][0][1][0])
+
+        print(f"Resizing to [{cuarto_parametro - tercer_parametro}, {segundo_parametro - primer_parametro}]")
+        marca_de_agua = cv2.resize(logo, (cuarto_parametro - tercer_parametro, segundo_parametro - primer_parametro))
+
+    if len(corners) > 0 \
+            and len(primer_plano[primer_parametro:segundo_parametro, tercer_parametro:cuarto_parametro]) > 0 \
+            and len(primer_plano[primer_parametro:segundo_parametro, tercer_parametro:cuarto_parametro][1]) > 0:
+        esquina_inferior = primer_plano[primer_parametro:segundo_parametro, tercer_parametro:cuarto_parametro] * inverso_alfa + minilog * alfa
+        primer_plano[primer_parametro:segundo_parametro, tercer_parametro:cuarto_parametro] = esquina_inferior
+
+    cv2.imshow('MEZCLA', background)
 
     k = cv2.waitKey(1)
     if k == ord('q'):
